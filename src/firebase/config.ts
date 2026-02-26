ecc522ea53927016b361943035ede485412efe47
Renan José de Oliveira <oliveiira2012@gmail.com>
2026-02-26 19:36:03 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5308015006-97323",
+  "appId": "1:119443718443:web:f329fbf207df212c2a056e",
+  "apiKey": "AIzaSyDWpkcBzTxzPzjHrwP-D2yjD_8lo8YV8S8",
+  "authDomain": "studio-5308015006-97323.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "119443718443"
+};
